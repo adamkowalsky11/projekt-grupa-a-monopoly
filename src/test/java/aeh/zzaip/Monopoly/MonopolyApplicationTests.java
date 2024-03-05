1c93bef593e6d21220d70523381ec880006aea73
Adam <adamkowalsky11@gmail.com>
2024-03-05 18:52:54 +0100

    initial commit

---
+++ src/test/java/aeh/zzaip/Monopoly/MonopolyApplicationTests.java
@@ -0,0 +1,13 @@
+package aeh.zzaip.Monopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
